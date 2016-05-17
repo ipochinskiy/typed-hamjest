440ee1d88d459d0cd6e7ff17334b7c502e7a3e1f
Mafo5 <mafo.work@gmail.com>
2016-05-17 14:46:07 +0200

    use spaces as indentation

--- test/test.ts
+++ test/test.ts
@@ -1,19 +1,19 @@
 describe('hamjest', () => {
-	it('should able to import by require', () => {
-		let __ = require('hamjest');
+    it('should able to import by require', () => {
+        let __ = require('hamjest');
 
-		__.assertThat(typeof __, __.is('object'));
-	});
+        __.assertThat(typeof __, __.is('object'));
+    });
 
-	describe('assertThat', () => {
-		let __: any;
-		before(() => {
-			__ = require('hamjest');
-		});
+    describe('assertThat', () => {
+        let __: any;
+        before(() => {
+            __ = require('hamjest');
+        });
 
-		it('should be defined', () => {
+        it('should be defined', () => {
 
-			__.assertThat(__.assertThat, __.defined());
-		});
-	});
+            __.assertThat(__.assertThat, __.defined());
+        });
+    });
 });
