27f72ee5c8b5c90ba595df04fa6f19976bb59838
Mafo5 <mafo.work@gmail.com>
2016-05-17 14:38:03 +0200

    add first test for assertThat

--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,17 @@
-import test = require('blue-tape');
+describe('hamjest', () => {
+	it('should be globally preset', () => {
+		let __ = require('hamjest');
+	});
 
+	describe('assertThat', () => {
+		let __: any;
+		before(() => {
+			__ = require('hamjest');
+		});
 
+		it('should be defined', () => {
+
+			__.assertThat(__.assertThat, __.defined());
+		});
+	});
+});
